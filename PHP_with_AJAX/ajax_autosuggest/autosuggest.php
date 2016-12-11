33f4415bca2e1504d5b2a0fe244ff3df6e681f68
Wahid Alimi <wahid.alimi@gmail.com>
2016-12-11 08:40:28 -0500

    7. Search autosuggest: Calculate results. (returning search results.)

--- PHP_with_AJAX/ajax_autosuggest/autosuggest.php
+++ PHP_with_AJAX/ajax_autosuggest/autosuggest.php
@@ -11,8 +11,53 @@
     exit;
   }
 
+
+  // Notes:
+  // * strpos is faster than strstr or preg_match
+  // * strpos requires strict comparison operator (===)
+  //   returns 0 for match at start of string
+  //   returns false for no match
+  function str_starts_with($choice, $query) {
+    return strpos($choice, $query) === 0;
+  }
+
+  // str_contains searches for the string anywhere in the data
+  function str_contains($choice, $query) {
+      return strpos($choice, $query) !== false;
+  }
+
+
+  // In real world, this would likely search a database, or use
+  // a search program like Solr, Elastic Search, Sphinx, etc.
+  function search($query, $choices) {
+    $matches = [];
+    $d_query = strtolower($query);
+
+    foreach($choices as $choice) {
+      // Lowercase both strings for case-insensitive search
+      $d_choice = strtolower($choice);
+
+      if(str_starts_with($d_choice, $d_query)) {
+        $matches[] = $choice;
+      }
+    }
+
+    return $matches;
+  }
+
   $query = isset($_GET['q']) ? $_GET['q'] : '';
 
   // find and return search suggestions as JSON
-  
+
+  // populate an array with list of names from the text file.
+  // (In the real world, this data would be coming from a DB.)
+  $choices = file('includes/us_passenger_trains.txt', FILE_IGNORE_NEW_LINES);
+
+  $suggestions = search($query, $choices);
+  sort($suggestions);
+  $max_suggestions = 5;
+  $top_suggestions = array_slice($suggestions, 0, $max_suggestions);
+
+  echo json_encode($top_suggestions);
+
 ?>
